9c22f63382df40b407f6b90aef02dcac5cb5b584
Santiago <santocrafter135135@gmail.com>
2024-04-12 22:02:54 -0300

    Creacion del proyecto

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start test on your app!</Text>
+      <Text>Gestion Departamento Alumnos App</Text>
       <StatusBar style="auto" />
     </View>
   );
